86d52f51a0c23aebce42aff4691cc51d252d1b62
unknown <gopinathanselvaraj21@gmail.com>
2023-10-08 18:37:24 +0530

    mailer added

--- functions/api.js
+++ functions/api.js
@@ -1,21 +1,41 @@
-const express = require('express');
-const serverless = require('serverless-http');
+const express = require("express");
+const serverless = require("serverless-http");
 const app = express();
 const router = express.Router();
-const nodemailer = require('nodemailer')
+const nodemailer = require("nodemailer");
 
-
-router.get('/', (req, res) => {
-  res.send('App is running..');
+router.get("/", (req, res) => {
+  res.send("App is running..");
 });
 
+router.post("/sendmail", (req, res) => {
+  const { to, subject, text } = req.body;
 
-router.post('/sendmail', (req, res) => {
-  res.send('New record added.');
+  const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: "gopinathanselvaraj21@gmail.com",
+      pass: "hrtxkhqjfjqwnlbd",
+    },
+  });
+
+  const mailOptions = {
+    from: "gopinathanselvaraj21@gmail.com",
+    to,
+    subject,
+    text,
+  };
+
+  transporter.sendMail(mailOptions,function(error , info){
+    if(error){
+      console.log('error' , error)
+    }
+    else{
+      console.log('Email sent' , info)
+    }
+  })
+
 });
 
-
-
-
-app.use('/.netlify/functions/api', router);
+app.use("/.netlify/functions/api", router);
 module.exports.handler = serverless(app);

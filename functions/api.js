c22d3ed8b3f8dc3560d86742dfb65ea729784274
unknown <gopinathanselvaraj21@gmail.com>
2023-10-09 13:01:21 +0530

    mail added

--- functions/api.js
+++ functions/api.js
@@ -3,7 +3,7 @@ const serverless = require("serverless-http");
 const app = express();
 const router = express.Router();
 const nodemailer = require("nodemailer");
-const bodyparser = require('body-parser');
+const bodyparser = require("body-parser");
 
 app.use(bodyparser.json());
 
@@ -11,35 +11,34 @@ router.get("/", (req, res) => {
   res.send("App is running..");
 });
 
-router.post("/sendmail", (req, res) => {
-  const { to, subject, text } = req.body;
+router.post("/sendmail", async (req, res) => {
+  const { to, subject, text, html } = req.body;
   console.log(req);
-  console.log(to , subject ,text);
+  console.log(to, subject, text);
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "Gmail",
+      auth: {
+        user: "gopinathanselvaraj21@gmail.com",
+        pass: "hrtxkhqjfjqwnlbd",
+      },
+    });
 
-  const transporter = nodemailer.createTransport({
-    service: "Gmail",
-    auth: {
-      user: "gopinathanselvaraj21@gmail.com",
-      pass: "hrtxkhqjfjqwnlbd",
-    },
-  });
+    const mailOptions = {
+      from: "gopinathanselvaraj21@gmail.com",
+      to: "gopinathanselvaraj21@gmail.com",
+      subject,
+      text,
+      html,
+    };
 
-  const mailOptions = {
-    from: "gopinathanselvaraj21@gmail.com",
-    to,
-    subject,
-    text,
-  };
-
-  transporter.sendMail(mailOptions,function(error , info){
-    if(error){
-      console.log('error' , error)
-    }
-    else{
-      console.log('Email sent' , info)
-    }
-  })
-
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent", info);
+    res.status(200).json({ message: "Email sent successfully" });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 app.use("/.netlify/functions/api", router);
